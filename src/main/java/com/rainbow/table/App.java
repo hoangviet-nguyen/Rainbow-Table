bf024da3df264503cb43356ae6bda1aaf5aedb63
hoangviet-nguyen <viety.nguyen02@gmail.com>
2024-04-23 11:45:28 +0200

    implemented the rainbow table

--- src/main/java/com/rainbow/table/App.java
+++ src/main/java/com/rainbow/table/App.java
@@ -1,10 +1,17 @@
 package com.rainbow.table;
 
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        //  TO DO MAKE METHOD TO FIND CLEAR TEXT
         String hash = "1d56a37fb6b08aa709fe90e12ca59e12";
         RainbowTable table = new RainbowTable();
+
+        String clearText = table.findClearTest(hash);
+        String test = table.hash("12e2feb5a0feccf82a8d4172a3bd51c3");
+        String r = table.reduce("12e2feb5a0feccf82a8d4172a3bd51c3", 1);
+
+        System.out.println("The clear Text to the given hash is: " + clearText);
+        System.out.println(test);
     }
 }    
--- src/main/java/com/rainbow/table/RainbowTable.java
+++ src/main/java/com/rainbow/table/RainbowTable.java
@@ -1,38 +1,71 @@
 package com.rainbow.table;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class RainbowTable {
     
     private List<String> passwords;
     private List<Character> characters;
     private Map<String, String> rainbowtable;
-    private List<Set<String>> hashReduceChain;
+    private List<Set<String>> hashChain;
+    private List<Set<String>> reduceChain;
+    private MessageDigest algorithm;
 
     public RainbowTable() {
         passwords = new ArrayList<>();
         characters = new ArrayList<>();
         rainbowtable = new HashMap<>();
-        hashReduceChain = new ArrayList<>();
+        hashChain = new ArrayList<>();
+        reduceChain = new ArrayList<>();
+
+        try {
+            algorithm = MessageDigest.getInstance("MD5");
+        } catch(NoSuchAlgorithmException nsae) {
+            System.out.println("The algorithm was not found exit");
+            throw new RuntimeException(nsae.getCause());
+        }
 
         fillCharacters();
         generatePasswords("0000000", 0, new AtomicInteger());
-        passwords.stream().limit(10).forEach(System.out::println);
+        initLayers();
+        initRainbowTable();
+
+        rainbowtable.keySet().stream().forEach(System.out::println);
     }
 
-    private String reduce(String hash, int layer) {
-        // TO DO IMPLEMENT METHOD
-        return null;
+    public String reduce(String hash, int layer) {
+
+        int passwordLenght = 7;
+        BigInteger hashValue = new BigInteger(hash, 16);
+        hashValue = hashValue.add(BigInteger.valueOf(layer));
+        String result = "";
+        BigInteger size = BigInteger.valueOf(characters.size());
+        
+        for(int i = 0; i < passwordLenght; i++) {
+            int index = hashValue.mod(size).intValueExact();
+            hashValue = hashValue.divide(size);
+            char r = characters.get(index);
+            result = r + result;
+        }
+         
+        return result;
     }
 
-    private String hash(String password) {
-        //TO DO IMPLEMENT METHOD
-        return null;
+    public String hash(String password) {
+        algorithm.update(password.getBytes());
+        byte[] digest = algorithm.digest();
+        BigInteger convertBytes = new BigInteger(1, digest);
+        String hashValue = convertBytes.toString(16);
+        return hashValue;
     }
 
     private void fillCharacters(){
@@ -70,18 +103,64 @@ public class RainbowTable {
 
     public int findHashLayer(String hash) {
         int currentLayer = 0;
-        for(Set<String> layer : hashReduceChain) {
+        for(Set<String> layer : hashChain) {
             if(layer.contains(hash)) {
                 return currentLayer;
             }
             currentLayer++;
         }
-
         return -1;
     }
 
+    public int findReduceLayer(String hash) {
+        int currentLayer = 0;
+        for(Set<String> layer : reduceChain) {
+            if(layer.contains(hash)) {
+                return currentLayer;
+            }
+            currentLayer++;
+        }
+        return -1;
+    }
+
+    private void initLayers(){
+        for(int i = 0; i < 2000; i++) {
+            hashChain.add(new HashSet<>());
+            reduceChain.add(new HashSet<>()); 
+        }
+    }
+
+    private void initRainbowTable() {
+        for(int i = 0; i < 1; i++) {
+
+            String password = passwords.get(i);
+            int overallLayer = 0;
+            int reduceLayer = 0;
+            int hashLayer = 0;
+            String value = hash(password);
+
+            // make the reduce and hash chain, each chain has 2000 entries
+            while (overallLayer < 2) {
+                
+                // first hash then reduce => hash has all even ints
+                if(overallLayer % 2 == 0) {
+                    hashChain.get(hashLayer).add(value);
+                    hashLayer++;
+                    value = reduce(value, reduceLayer);
+                } else {
+                    reduceChain.get(reduceLayer).add(value);
+                    reduceLayer++;
+                    value = hash(value);
+                }
+                overallLayer++;
+            }
+
+            // the last hash value is stored in a map for easy access
+            rainbowtable.put(hash(value), password);
+        }
+    }
+
     private String followChain(String hash, int layer) {
-        // TO DO IMPLEMENT METHOD
         return null;
     }
 
